2143606d5b8df27e37dc32b8dd328848ac4363ec
kohunglee <33373536+kohunglee@users.noreply.github.com>
2022-02-05 18:13:17 +0800

    add the 'founder' id

--- hljs/hljs_customStyleHtml.php
+++ hljs/hljs_customStyleHtml.php
@@ -5,7 +5,7 @@
 
 //判断是否是管理员登录状态
 require_once '../../../init.php';
-!(ROLE == 'admin') && exit('非管理员权限！');
+!(ROLE == ('admin' || 'founder')) && exit('非管理员权限！');
 error_reporting(0);
 
 $plugin_storage = Storage::getInstance('plugin_hljs');  // 初始化emlog插件存储实例 plugin_hljs
